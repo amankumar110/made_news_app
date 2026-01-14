066c56cd700ca91567274923dd706d636c0b3f01
aman kumar <amantalreja110@gmail.com>
2026-01-14 18:55:17 +0500

    Update API_KEY in Constants.kt

--- app/src/main/kotlin/in/amankumar110/madenewsapp/utils/Constants.kt
+++ app/src/main/kotlin/in/amankumar110/madenewsapp/utils/Constants.kt
@@ -3,6 +3,6 @@ package `in`.amankumar110.madenewsapp.utils
 object Constants {
 
     const val BASE_URL = "https://made-news-app-server.vercel.app"
-    const val API_KEY = "88479a30-8e6d-4ef1-a367-21f8b27ab46a"
+    const val API_KEY = "a1b3c8d9f0e24b6d9c8a7f3e4d2c1f0b6a8c9d1e2f3a4b5c6d7e8f9a0b1c2d3e"
     const val IMBB_API_KEY = "f39930b1242a42d296fdfb71d5d68f56"
 }
