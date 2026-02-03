baaae84fcd82b69f2e13577c0197caec91baf585
amanKumar110 <amantalreja110@gmail.com>
2026-02-03 18:50:40 +0500

    changed base url

--- app/src/main/kotlin/in/amankumar110/madenewsapp/utils/Constants.kt
+++ app/src/main/kotlin/in/amankumar110/madenewsapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package `in`.amankumar110.madenewsapp.utils
 
 object Constants {
 
-    const val BASE_URL = "https://made-news-app-server.vercel.app"
+    const val BASE_URL = "https://made-news-server.vercel.app/"
     const val API_KEY = "a1b3c8d9f0e24b6d9c8a7f3e4d2c1f0b6a8c9d1e2f3a4b5c6d7e8f9a0b1c2d3e"
     const val IMBB_API_KEY = "f39930b1242a42d296fdfb71d5d68f56"
 }
